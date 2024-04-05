df3c02debaa39764be6e814967e31b6f060c58f4
Juan Saldana <saldanaj97@gmail.com>
2024-04-04 22:10:05 -0500

    mock calls setup

--- src/__mocks__/axiosMocks.ts
+++ src/__mocks__/axiosMocks.ts
@@ -1,12 +1,14 @@
+import listOfAllCars from '../__mocks__/test-data/carList.json'
+import detailedSeriesData from '../__mocks__/test-data/detailedSeriesData.json'
 import generalizedSeriesData from '../__mocks__/test-data/generalizedSeriesData.json'
 import successfulAuthResponse from '../__mocks__/test-data/successfulAuth.json'
 
 // Mock the post method of Axios
-export const axiosMockPost = jest.fn().mockResolvedValueOnce({
+export const apiAuthPostMock = jest.fn().mockResolvedValueOnce({
   data: successfulAuthResponse,
 })
 
-export const axiosMockGetSeries = jest
+export const getGeneralizedSeriesDataMock = jest
   .fn()
   .mockResolvedValueOnce({
     data: {
@@ -16,5 +18,31 @@ export const axiosMockGetSeries = jest
   .mockResolvedValueOnce({
     data: {
       all_series_available: generalizedSeriesData,
+    },
+  })
+
+export const getDetailedSeriesDataMock = jest
+  .fn()
+  .mockResolvedValueOnce({
+    data: {
+      link: 'https://example.com/detailedSeriesData',
+    },
+  })
+  .mockResolvedValueOnce({
+    data: {
+      detailed_series_data: detailedSeriesData,
+    },
+  })
+
+export const getListOfAllCarsMock = jest
+  .fn()
+  .mockResolvedValueOnce({
+    data: {
+      link: 'https://example.com/detailedSeriesData',
+    },
+  })
+  .mockResolvedValueOnce({
+    data: {
+      cars: listOfAllCars,
     },
   })
