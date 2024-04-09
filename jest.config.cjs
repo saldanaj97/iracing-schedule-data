bcab5c44e25ebe6858e48701ed7fddbbe1283cbc
Juan Saldana <saldanaj97@gmail.com>
2024-04-09 11:50:47 -0500

    fresh start

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['./src'],
-}
