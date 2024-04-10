58ff187443ca1b493dfbb7d2b01681357d256700
Juan Saldana <saldanaj97@gmail.com>
2024-04-10 12:34:09 -0500

    Add ESLint configuration file

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import pluginJs from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  { ignores: ["node_modules/*", "dist/*", "**/.*"] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+]
