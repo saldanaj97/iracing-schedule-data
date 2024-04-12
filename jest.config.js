8ea6b9c67b6483b01ab2f224d5f7099f3cc056c7
Juan Saldana <saldanaj97@gmail.com>
2024-04-11 22:09:21 -0500

    Update .gitignore and jest.config.js, remove unused mock data and files, and update tsconfig.json and package.json

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  preset: "ts-jest",
   transform: { "^.+\\.ts?$": "ts-jest" },
   testEnvironment: "node",
-  testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
+  testRegex: "/__tests__/.*\\.(test|spec)?\\.(ts|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFiles: ["<rootDir>/__tests__/test-setup.ts"],
 }
