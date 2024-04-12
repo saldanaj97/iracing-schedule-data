0df5392475aeb8601e8fbb921241c7873eca6dbe
Juan Saldana <saldanaj97@gmail.com>
2024-04-12 13:16:44 -0500

    split functions from index for less clutter in main file

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+import { ConstantType } from "./types"
+import { client } from "./utils/axiosSetup"
+
+/**
+ * Function that will grab all the constants available on the API
+ * @param constant - The constant you want to retrieve (categories, car_classes, event_types)
+ * @returns A list containing each constant and its corresponding value
+ */
+export const getConstants = async ({
+  constant,
+}: {
+  constant: "categories" | "car_classes" | "event_types"
+}): Promise<ConstantType[] | undefined> => {
+  const URL = `https://members-ng.iracing.com/data/constants/${constant}`
+  try {
+    const constants = await client.get(URL).then((response) => response.data)
+    return constants
+  } catch (error) {
+    console.error(error)
+    return undefined
+  }
+}
