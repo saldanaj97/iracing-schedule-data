57d8f3bc07c9dfdae64272928206c1d732033a12
Juan Saldana <saldanaj97@gmail.com>
2024-05-01 14:24:56 -0500

    Fix boolean parameter conversion in appendParams.ts

--- src/utils/appendParams.ts
+++ src/utils/appendParams.ts
@@ -1,6 +1,9 @@
 export const appendParams = (URL: string, params: { [paramName: string]: any }) => {
   Object.entries(params).forEach(([paramName, paramValue]) => {
     if (paramValue !== undefined) {
+      if (typeof paramValue === "boolean") {
+        paramValue = paramValue ? 1 : 0
+      }
       URL += `&${paramName}=${paramValue}`
     }
   })
