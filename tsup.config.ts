1c537a96769ab728929fc9ec922f20901ad7a8ab
Juan Saldana <saldanaj97@gmail.com>
2024-05-02 22:13:05 -0500

    chore: Update entry point and exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: [
-    "src/auth/auth.ts",
-    "src/car/car.ts",
-    "src/constants/constants.ts",
-    "src/hosted/hosted.ts",
-    "src/league/league.ts",
-    "src/lookup/lookup.ts",
-    "src/member/member.ts",
-    "src/results/results.ts",
-    "src/season/season.ts",
-    "src/series/series.ts",
-    "src/time_attack/timeAttack.ts",
-    "src/track/track.ts",
-    "src/utils/*.ts",
-  ],
+  entry: ["src/index.ts"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
