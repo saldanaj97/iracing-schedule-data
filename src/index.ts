494195718f79d5d53cbce32ef6fe839c87c6a2f4
Juan Saldana <saldanaj97@gmail.com>
2024-05-02 19:49:20 -0500

    removed unused test code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,1 @@
-import dotenv from "dotenv"
-import { fetchAuthCookie } from "./auth/auth"
 
-// ENV for test environment
-dotenv.config()
-
-const main = async () => {
-  const username = process.env.IRACING_USERNAME
-  const password = process.env.IRACING_PASSWORD
-
-  if (!username || !password) {
-    throw new Error("Please provide a username and password in the .env file")
-  }
-
-  const auth = await fetchAuthCookie({ username: username, password: password })
-  console.log(auth)
-}
-
-main()
